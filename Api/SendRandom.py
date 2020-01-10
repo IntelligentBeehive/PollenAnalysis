345090d6cda7e536317bf98e63cc73a367a26086
Hugo Mkandawire <335167@student.fontys.nl>
2020-01-10 10:12:35 +0100

    Update: sendrandom checkdate and dice

--- Api/SendRandom.py
+++ Api/SendRandom.py
@@ -1,8 +1,11 @@
 import csv
 import json
 import random
+from datetime import datetime
+
 import requests
-import re
+from termcolor import colored
+import time
 
 
 def make_record(row):
@@ -24,20 +27,36 @@ def send_request(json_dict):
 def random_date():
     year = random.randint(2014, 2019)
     month = random.randint(1, 12)
-    if month in (1, 3, 5, 7, 8, 10, 12):
-        day = random.randint(1, 31)
-    elif month == 2:
-        day = random.randint(1, 28)
-    else:
-        day = random.randint(1, 30)
+    day = random.randint(1, 31)
     hour = random.randint(0, 23)
     minute = random.randint(0, 59)
     second = random.randint(0, 59)
-    date = str(year) + '-' + str(month) + '-' + str(day) + ' ' + str(hour) + ':' + str(minute) + ':' + str(second)
-    return date
+
+    # Check if the date is valid
+    try:
+        # year = 2015
+        # month = 3
+        # day = 29;
+        # hour = 2
+        # minute = 1
+        # second = 1
+        date = str(year) + '-' + add_zero(month) + '-' + add_zero(day) + ' ' + add_zero(
+            hour) + ':' + add_zero(minute) + ':' + add_zero(second)
+        datetime(year, month, day, hour, minute, second)
+    except ValueError as ve:
+        print(colored(f"# date '{date}' is invalid, because {ve} #", 'red'))
+        date = random_date()  # Date is invalid, get another date!
+
+    return date  # Date is valid!
 
 
-withDate = False
+def add_zero(number):
+    if number < 10:
+        return "0" + str(number)
+    return str(number)
+
+
+withDate = True
 
 with open('..\CSV\pollenchart.csv', 'r', newline='') as csvfile:
     reader = csv.DictReader(csvfile, delimiter=';')
@@ -48,14 +67,18 @@ with open('..\CSV\pollenchart.csv', 'r', newline='') as csvfile:
 with open('json_file.json', 'r') as jsonfile:
     print('results:')
     dump = json.load(jsonfile)
+    flowers = []
+    for i in range(0, 10):
+        flowers.append(random.choice(dump))
+
     counter = 1
-    while counter <= 1000:
-        choice = random.choice(dump)
+    dice = 1
+    while counter <= 50000:
+        choice = random.choice(flowers)
         if withDate:
-            dice = 1
             choice["dateCreated"] = random_date()
         else:
-            dice = random.randint(1, 10000000)
+            dice = random.randint(1, 100000)
         if dice == 1:
             print(str(counter) + ':')
             send_request(choice)
